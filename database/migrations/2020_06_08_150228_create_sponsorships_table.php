73c89cc6ebf3d099831c42ea8c78dcda97a20123
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-06-08 17:26:32 +0200

    migrations

--- database/migrations/2020_06_08_150228_create_sponsorships_table.php
+++ database/migrations/2020_06_08_150228_create_sponsorships_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSponsorshipsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sponsorships', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('surname', 50);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->date('date_of_birth');
+            $table->foreignId('apartment_id')->constrained();
+            $table->dateTime('sponsor_end', 0);
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sponsorships');
     }
 }
--- database/migrations/2020_06_08_152004_create_services_table.php
+++ database/migrations/2020_06_08_152004_create_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('surname', 50);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->date('date_of_birth');
+            $table->boolean('wifi');
+            $table->boolean('parking_space');
+            $table->boolean('pool');
+            $table->boolean('reception');
+            $table->boolean('sauna');
+            $table->boolean('sea_view');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 }
