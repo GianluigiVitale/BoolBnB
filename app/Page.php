91d8f6acabb35a6f7fd3c97b4bc0a43243b39ffa
Federico <federicobartoli@me.com>
2020-06-09 16:28:36 +0200

    Branch contatore

---
+++ app/Page.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use CyrildeWit\EloquentViewable\InteractsWithViews;
+use CyrildeWit\EloquentViewable\Contracts\Viewable;
+
+class Page extends Model implements Viewable
+{
+    use InteractsWithViews;
+
+}
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/page', 'PageController@show')->name('page');
