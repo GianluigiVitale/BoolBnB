7414ef6619f59f10924caa2c3b6890355147f76f
Michael Hernandez <gammabeta010@gmail.com>
2020-06-16 12:55:50 +0200

    Changes to search to return apts from DB

--- public/js/app.js
+++ public/js/app.js
@@ -37246,13 +37246,61 @@ var placesAutocomplete = places({
   apiKey: '593f74e1b84067335abcb422c5d9d7bd',
   container: document.querySelector('#address-input')
 }); // var coordinates = placesAutocomplete.on('change', e => console.log([e.suggestion['latlng'].lat,e.suggestion['latlng'].lng]));
+// placesAutocomplete.on('change', e => $('#latitude').val(e.suggestion['latlng'].lat));
+// placesAutocomplete.on('change', e => $('#longitude').val(e.suggestion['latlng'].lng));
 
 placesAutocomplete.on('change', function (e) {
-  return $('#latitude').val(e.suggestion['latlng'].lat);
-});
-placesAutocomplete.on('change', function (e) {
-  return $('#longitude').val(e.suggestion['latlng'].lng);
-}); //DATEPICKER
+  $('#longitude').val(e.suggestion['latlng'].lng);
+  $('#latitude').val(e.suggestion['latlng'].lat);
+  var tempLong = e.suggestion['latlng'].lng;
+  var tempLat = e.suggestion['latlng'].lat;
+  console.log(tempLong, tempLat);
+  $.ajax({
+    'url': '/api/apartments',
+    'method': 'GET',
+    'success': function success(data) {
+      //console.log(data);
+      for (var key in data) {
+        //console.log(data[key]['latitude']);
+        var dist = distance(tempLat, tempLong, data[key]['latitude'], data[key]['longitude'], 'K');
+        console.log(dist);
+      } //var dist = distance(tempLat, tempLong,)
+
+    }
+  });
+}); //console.log($('#longitude').val());
+// lat1, lon1 == search
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    var radlat1 = Math.PI * lat1 / 180;
+    var radlat2 = Math.PI * lat2 / 180;
+    var theta = lon1 - lon2;
+    var radtheta = Math.PI * theta / 180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+
+    if (dist > 1) {
+      dist = 1;
+    }
+
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+
+    if (unit == "K") {
+      dist = dist * 1.609344;
+    }
+
+    if (unit == "N") {
+      dist = dist * 0.8684;
+    }
+
+    return dist;
+  }
+} //DATEPICKER
+
 
 var datepicker = __webpack_require__(/*! js-datepicker */ "./node_modules/js-datepicker/dist/datepicker.min.js");
 
