d401ff6533a2c605b88b6b056a8e6e0fe3538e49
Federico <federicobartoli@me.com>
2020-06-17 14:04:52 +0200

    Front-end Homepage in progress

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,10 +1,17 @@
 <main>
-       <input type="search" id="address-input" class="form-control" placeholder="Where are we going?" />
 
-       @foreach ($services as $service)
-       <div id="service-list" class="form-check">
-              <input class="form-check-input" type="checkbox" value="{{$service->id}}" id="defaultCheck1">
-              <label class="form-check-label" for="defaultCheck1">{{$service->service_name}}</label>
+
+       <div class="container-fluid bb-jumbo">
+                <div class="bb-jumbo-main">
+                    <input type="search" id="address-input" class="form-control" placeholder="Where are we going?" />
+                        <div class="bb-flex">
+                            @foreach ($services as $service)
+                            <div id="service-list" class="form-check ">
+                                   <input class="form-check-input " type="checkbox" value="{{$service->id}}" id="defaultCheck1">
+                                   <label class="form-check-label " for="defaultCheck1">{{$service->service_name}}</label>
+                            </div>
+                            @endforeach
+                        </div>
+                </div>
        </div>
-       @endforeach
 </main>
